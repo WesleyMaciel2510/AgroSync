00c21ecaf965715a7370468807b2be6eca21617d
WesleyMaciel2510 <wesley.2510@hotmail.com>
2024-03-19 14:32:14 -0300

    pictures being sent correctly in invoicesInfoScreen
    mend

--- src/context/globalUseState.tsx
+++ src/context/globalUseState.tsx
@@ -9,8 +9,10 @@ export const useStateVariables = () => {
   const [password, setPassword] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
-  const [userType, setUserType] = useState('Motorista');
-  const [isLogged, setIsLogged] = useState(true);
+  //const [userType, setUserType] = useState('Motorista');
+  const [userType, setUserType] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  //const [isLogged, setIsLogged] = useState(true);
   const [cameraPermission, setCameraPermission] = useState(false);
   const [savePermission, setSavePermission] = useState(false);
   const [photo, setPhoto] = useState<string | undefined>(undefined);
