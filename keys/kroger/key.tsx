f84789de48075be0d3602f3c7b93427393050ab1
WesleyMaciel2510 <wesley.2510@hotmail.com>
2024-04-26 14:35:59 -0300

    last apk with keystore generated working

--- keys/kroger/key.tsx
+++
@@ -1,4 +0,0 @@
-export const ClientID =
-  'agrosync-8446b95a9870cc7e2836c29fc3e6fd3c4076060520800214714';
-
-export const ClientSecret = 'EIAFbMEOWPNWpUc4FDPiG7AbcU_cR0klM-qMjULq';
--- keys/spoonacular/key.tsx
+++
@@ -1,1 +0,0 @@
-export const privateKey: string = 'e024d52b2f994371bcd55b2e31f47d99';
